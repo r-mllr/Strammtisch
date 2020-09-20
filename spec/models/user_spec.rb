1f49717d93ccfdb1ce012f24cf30eb42f67a8a58
Fabian Müller <fmueller@secondmeasure.com>
2020-09-20 17:12:44 +0200

    Adds user factory and test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'can create user' do
+    expect(FactoryBot.create(:user)).to be_a(User)
+  end
 end
