90397cd097ca0f0e014131f1df2cce2ba983c24f
Fabian Müller <fmueller@secondmeasure.com>
2020-09-20 17:54:49 +0200

    Setup adminsitrate for users and events

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :users
+      resources :events
+
+      root to: "users#index"
+    end
   devise_for :users
   get 'pages/home'
 
