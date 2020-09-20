1ded07da4c902b64adfea6e77a0bad42b3d19343
Raphael Müller <raphaelmueller06@gmail.com>
2020-09-20 18:16:30 +0200

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -37,8 +37,10 @@ ActiveRecord::Schema.define(version: 2020_09_20_152418) do
     t.datetime "locked_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
 end
