8d9a599fd552e6fa324a030d3428480e0042ac38
Raphael Müller <raphaelmueller06@gmail.com>
2020-09-21 20:11:27 +0200

    authenticate admins for admin dashboards

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -6,7 +6,12 @@
 # you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
-    before_action :authenticate_user!
+    before_action :authenticate_admin
+
+    def authenticate_admin
+      authenticate_user!
+      redirect_to root_path, alert: "Not authorized!" unless current_user.admin?
+    end
 
     # Override this value to specify the number of elements to display at a time
     # on index pages. Defaults to 20.
