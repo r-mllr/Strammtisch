a03c3808637d6f5bd384dfdc57284d832a42b8bc
Raphael Müller <raphaelmueller06@gmail.com>
2020-09-21 19:50:59 +0200

    updated user dashboard

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -11,6 +11,8 @@ class UserDashboard < Administrate::BaseDashboard
     id: Field::Number,
     email: Field::String,
     encrypted_password: Field::String,
+    password: Field::Password,
+    password_confirmation: Field::Password,
     reset_password_token: Field::String,
     reset_password_sent_at: Field::DateTime,
     remember_created_at: Field::DateTime,
@@ -31,10 +33,10 @@ class UserDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-  id
   email
-  encrypted_password
-  reset_password_token
+  confirmed_at
+  locked_at
+  created_at
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
@@ -42,16 +44,11 @@ class UserDashboard < Administrate::BaseDashboard
   SHOW_PAGE_ATTRIBUTES = %i[
   id
   email
-  encrypted_password
-  reset_password_token
   reset_password_sent_at
-  remember_created_at
-  confirmation_token
   confirmed_at
   confirmation_sent_at
   unconfirmed_email
   failed_attempts
-  unlock_token
   locked_at
   created_at
   updated_at
@@ -62,17 +59,9 @@ class UserDashboard < Administrate::BaseDashboard
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
   email
-  encrypted_password
-  reset_password_token
-  reset_password_sent_at
-  remember_created_at
-  confirmation_token
   confirmed_at
-  confirmation_sent_at
-  unconfirmed_email
-  failed_attempts
-  unlock_token
-  locked_at
+  password
+  password_confirmation
   ].freeze
 
   # COLLECTION_FILTERS
@@ -90,7 +79,7 @@ class UserDashboard < Administrate::BaseDashboard
   # Overwrite this method to customize how users are displayed
   # across all pages of the admin dashboard.
   #
-  # def display_resource(user)
-  #   "User ##{user.id}"
-  # end
+  def display_resource(user)
+     "User #{user.email}"
+   end
 end
