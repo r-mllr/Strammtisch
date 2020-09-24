d1bfafc3e47b6c0d496a38019a0145c748f59daf
Raphael Müller <raphaelmueller06@gmail.com>
2020-09-24 20:22:19 +0200

    added scope for past and upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,12 @@
 class Event < ApplicationRecord
   validates :takes_place_at, presence: true
+  
+  scope :upcoming, -> { 
+    now = Time.zone.now
+    where("takes_place_at >= ?", now) 
+  }
+  scope :past, -> {
+    now = Time.zone.now
+    where("takes_place_at < ?", now)
+  }
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -10,4 +10,38 @@ RSpec.describe Event, type: :model do
       expect(event_without_time).not_to be_valid
     end
   end
+
+  context 'scopes' do
+    describe 'future events' do
+      let!(:future_event) {FactoryBot.create(:event, takes_place_at: 100.years.from_now )}
+      let!(:past_event) {FactoryBot.create(:event, takes_place_at: 100.years.ago )}
+
+      it "has future events" do
+        expect(Event).to respond_to :upcoming
+      end
+
+      it "shows only future events" do
+        future_events = Event.upcoming
+        expect(future_events.size).to eq 1
+        expect(future_events).to include(future_event)
+        expect(future_events).not_to include(past_event)
+      end
+    end
+
+    describe 'past events' do
+      let!(:future_event) {FactoryBot.create(:event, takes_place_at: 100.years.from_now )}
+      let!(:past_event) {FactoryBot.create(:event, takes_place_at: 100.years.ago )}
+
+      it "has past events" do
+        expect(Event).to respond_to :past
+      end
+
+      it "shows only past events" do
+        past_events = Event.past
+        expect(past_events.size).to eq 1
+        expect(past_events).not_to include(future_event)
+        expect(past_events).to include(past_event)
+      end
+    end
+  end
 end
