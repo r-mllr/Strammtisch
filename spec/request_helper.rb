7f228c58ac5c404213a16ca38286b48651376e83
Raphael Müller <raphaelmueller06@gmail.com>
2020-09-24 19:55:39 +0200

    added helper for loggin out

--- spec/request_helper.rb
+++ spec/request_helper.rb
@@ -3,3 +3,7 @@
 def login(user, password = 'password')
   post user_session_path, params: { 'user[email]' => user.email, 'user[password]' => password }
 end
+
+def logout
+  delete destroy_user_session_path
+end
