b57e5d9045fb9da3fca4a35bba23565156e96b23
Raphael Müller <raphaelmueller06@gmail.com>
2020-09-24 20:54:32 +0200

    only unique mail addresses are generated

--- spec/factories/devise.rb
+++ spec/factories/devise.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    email { 'test@user.com'}
+    email { Faker::Internet.unique.email }
     password {'password'}
     password_confirmation {'password'}
     confirmed_at {Time.zone.now}
